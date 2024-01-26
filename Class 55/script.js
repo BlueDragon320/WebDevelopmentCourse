37fea122228ccbbca1ec286230213830bfdcca73
Vaibhav <120330097+BlueDragon320@users.noreply.github.com>
2024-01-26 17:22:34 +0530

    added class 55

---
+++ Class 55/script.js
@@ -0,0 +1,41 @@
+console.log("This is the secound video of java script")
+
+var a = 5;
+// a = a + 1
+let b = 6;
+let _a = 69;
+
+console.log(a+b);
+console.log(typeof a, typeof b, typeof _a);
+
+// const a1 = 6;
+// a1 = a1 + 1; Not allowed as a1 is a constant
+
+{
+    let a = 1;
+    console.log(a   )
+}
+
+
+let x = "Pintya"
+let y = 22;
+let z = 3.44;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+let o = {
+    name: "Chintu",
+    "job role": 6969,
+    "is_handsome": true,
+}
+
+console.log(o);
+o.salary="100crores";
+console.log(o);
+o.salary="500crores";
+console.log(o)
