781a18413684053770c19c2771d1868b1a25440c
Vaibhav <120330097+BlueDragon320@users.noreply.github.com>
2024-01-26 14:45:50 +0530

    added Class 52 nodejs

---
+++ Video 54/script.js
@@ -0,0 +1,17 @@
+alert("Hello World")
+
+console.log("Hey there I am a console")
+console.log("Hey there I am also console")
+console.log("Hey there I am the best console")
+
+var a = prompt("Enter a number")
+var isTrue = confirm("Are you sure you want to continue this rat race")
+
+if(isTrue){
+    console.log("Congractulations you are addicted to salary")
+}
+else{
+    console.log("Congractulations you can get into top 0.1% with 100% of efforts and good decisions")
+}
+
+console.log("Your number is " + a)
