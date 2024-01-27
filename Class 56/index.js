af5322c903ad163f7df3561fec74f1f1856ef851
Vaibhav <120330097+BlueDragon320@users.noreply.github.com>
2024-01-27 20:15:15 +0530

    Completed Class 56

---
+++ Class 56/index.js
@@ -0,0 +1,22 @@
+console.log("Hello I am conditional tutorial")
+
+let age = 17;
+let grace = 2;
+
+age += grace
+
+console.log(age)
+console.log(age + grace)
+console.log(age ** grace)
+console.log(age % grace)
+
+
+// if((age+grace)>18)
+//     console.log("You can drive ");
+
+if(age>18){
+    console.log("You can drive ")
+}
+else{
+    console.log("Sorry, you can't drive")
+}
---
+++ Class 56/index2.js
@@ -0,0 +1,30 @@
+age = 17
+
+grace = 1
+
+if(age>=18){
+    console.log("You can drive")
+}
+else if(age==17){
+    console.log("Be patient for an year to drive")
+}
+else if(age<=14){
+    console.log("You got be joking")
+}
+
+/*
+let a=6;
+let b=9;
+
+if(a>b){
+    let c=a-b;
+}
+else {
+    let c=b-a;
+}
+
+or
+
+c=a>b ?(a-b):(b-a)
+
+*/
