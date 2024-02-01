7316b99b3499a0fe8b58907f932ada06f0fa1108
Vaibhav <120330097+BlueDragon320@users.noreply.github.com>
2024-02-01 13:14:05 +0530

    Completed Exercise 9

--- Exercise 9/script.js
+++ Exercise 9/script.js
@@ -7,9 +7,15 @@ let obj = {
     "+": "-",
     "*": "+",
     "-": "/",
-    "/": "**,"
+    "/": "**",
 }
 
 if(random>0.1){
-    
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+
+}
+
+else {
+    c = obj[ca]
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
 }
