cf9808f6fb7c2516c3183d1b7a1bb1c46091b8aa
Vaibhav <120330097+BlueDragon320@users.noreply.github.com>
2024-02-07 23:13:11 +0530

    Completed Video 67

--- Video 67/script.js
+++ Video 67/script.js
@@ -1,1 +1,9 @@
-// console.log("Hello world")
+console.log("Hello world")
+
+// makes work easier 'Let cont = document.body.firstElementChild'  then using cont = to the code written
+
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children
+
+document.body.firstElementChild[3].nextElementSibling
