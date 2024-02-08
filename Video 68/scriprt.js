91e8dd16f482a7a4640fd6601cc909358af2b4ed
Vaibhav <120330097+BlueDragon320@users.noreply.github.com>
2024-02-08 08:12:33 +0530

    Completed Video 68

---
+++ Video 68/scriprt.js
@@ -0,0 +1,16 @@
+console.log("CHintu OP")
+
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+
+// boxes[2].style.backgroundColor = "aqua"
+
+// document.getElementById("aqua").style.backgroundColor = "aqua"
+
+// document.querySelector(".box").style.backgroundColor = "green ";
+
+// console.log(document.querySelectorAll(".box"))
+
+// document.querySelectorAll(".box").forEach(e => {
+//     e.style.backgroundColor = "blue";
+// })
