a5b9b402fdf786f822bba39eae5baacced10203b
Vaibhav <120330097+BlueDragon320@users.noreply.github.com>
2024-02-10 23:08:32 +0530

    COmpleted Exercise 11

--- Exercise 11/script.js
+++ Exercise 11/script.js
@@ -1,8 +1,19 @@
-let a = 6 
+let a = 7
 
-function factorial(number){
+function factorial(number) {
     let arr = Array.from(Array(number).keys())
-    console.log.arr
+    let c = arr.slice(1,).reduce((a, b) => a * b)
+    return c
 }
 
-factorial(a)
+function facFor(number) {
+    let fac = 1;
+    for (let index = 1; index <= number; index++) {
+        fac = fac * index
+    }
+    return fac
+}
+console.log(factorial(a))
+console.log(facFor(a))
+
+
