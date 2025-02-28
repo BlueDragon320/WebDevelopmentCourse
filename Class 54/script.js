50ece8e11669f91b1aeb94a44e3d96d0c6e9158a
Blue <120330097+BlueDragon320@users.noreply.github.com>
2025-02-28 23:25:00 +0530

    Add files via upload

---
+++ Class 54/script.js
@@ -0,0 +1,16 @@
+alert("hello world");
+
+console.log("Hello bhai")
+
+var a = prompt("Enter your number")
+console.log("Your number is ${a}")
+var isTrue = confirm("Are you sure?")
+
+if(isTrue){
+    console.log("You done mate")
+}
+else{
+    console.log("you are fine mate")
+}
+
+document.title = "It's me"
