197b5772fd84d5d10c1ac4bed87b9971b41d2ca8
Blue <120330097+BlueDragon320@users.noreply.github.com>
2025-03-01 23:34:35 +0530

    Add files via upload

---
+++ Class 55 [Again]/script.js
@@ -0,0 +1,21 @@
+// var a = 5;
+// var b = 6;
+// console.log(a+b+8);
+// console.log(typeof a+ typeof b);
+
+
+let x = "EHhhh";
+let y = 23;
+let z = 33.44;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+
+let o = {
+    namm : "Harry",
+    "job role": " Dev 5600",
+}
+console.log(o);
