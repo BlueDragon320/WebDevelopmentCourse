8d6283be22a29bad1e59a3b187c56c64e8d3782e
Blue <120330097+BlueDragon320@users.noreply.github.com>
2025-09-16 23:41:49 +0530

    Add files via upload

---
+++ Class 55 Again/script.js
@@ -0,0 +1,18 @@
+console.log("This is tutorial 55")
+var a = 5;
+var b = 6;
+console.log(a + b + b)
+console.log(typeof a, typeof b)
+
+
+
+// Objects in JS
+
+let obj = {
+    "name" : "ramu",
+    "age" : 20,
+    "city" : "Dilli",
+}
+console.log(obj)
+obj.salary = "100 crores"
+console.log(obj)
